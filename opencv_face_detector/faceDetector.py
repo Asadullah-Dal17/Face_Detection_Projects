1d4b4f7663d3b5e3a19afa6756a5a42376c5191c
Asadullah-Dal17 <aiphile17@gmail.com>
2021-05-31 13:29:59 +0500

    face detection complete

--- opencv_face_detector/faceDetector.py
+++ opencv_face_detector/faceDetector.py
@@ -6,7 +6,11 @@ faceDetector = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 camera = cv.VideoCapture(0)
 
 while True:
-    ret, frame = cap.read()
+    ret, frame = camera.read()
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    faces = faceDetector.detectMultiScale(gray, 1.3, 5)
+    for (x, y, h, w) in faces:
+        cv.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
 
     cv.imshow('image', frame)
     key = cv.waitKey(1)
